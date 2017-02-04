86c19317757a3f462bfbcf624e39f8fedef07c1b
jwowillo <will.orourke10@gmail.com>
2017-02-04 17:41:08 -0600

    Add Slider Swapping

--- static/slider.component.ts
+++ static/slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule, OnInit, ViewEncapsulation} from '@angular/core'
+import {Component, NgModule, OnInit, ViewEncapsulation, Input, Output, EventEmitter} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {Rover, RoverManifest} from './entity/Rover';
 import {RoverService} from './service/rover.service';
@@ -10,6 +10,33 @@ import {RoverService} from './service/rover.service';
 })
 
 export class SliderComponent implements OnInit {
+
+  rovers: Rover[] = [];
+
+  @Input()
+  mainView: string = "";
+
+  @Output()
+  mainViewChange:EventEmitter<String> = new EventEmitter<String>();
+
+  @Input()
+  rover: string = "";
+
+  @Output()
+  roverChange:EventEmitter<String> = new EventEmitter<String>();
+
+  @Input()
+  sol: number = 0;
+
+  @Output()
+  solChange:EventEmitter<Number> = new EventEmitter<Number>();
+
+  @Input()
+  camera: string = "";
+
+  @Output()
+  cameraChange:EventEmitter<String> = new EventEmitter<String>();
+
 
   constructor(private _roverService: RoverService) {
 
@@ -24,4 +51,9 @@ export class SliderComponent implements OnInit {
       this.rovers = rovers;
     });
   }
+
+  toCaps(rover) {
+    return rover.split(' ').map(i => i[0].toUpperCase() + i.substr(1).toLowerCase());
+  }
+
 }
