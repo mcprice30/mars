4949f88edddd27504df70a1543bd85f6be48b694
jwowillo <will.orourke10@gmail.com>
2017-02-03 23:24:16 -0600

    Add Documentation for Rest of Controllers

--- api/rovers.go
+++ api/rovers.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/jwowillo/trim"
 	"github.com/jwowillo/trim/response"
 )
@@ -24,14 +22,14 @@ func (c *roversController) Path() string {
 	return roversPath
 }
 
-// Handle the trim.Request by creating a mapping of all rovers to their
+// Handle the trim.Request by returning a mapping of all rovers to their
 // roverResource paths.
 func (c *roversController) Handle(r *trim.Request) trim.Response {
 	m := trim.AnyMap{}
 	for _, r := range rovers {
 		m[r] = makeRoverPath(r)
 	}
-	return response.NewJSON(m, http.StatusOK)
+	return response.NewJSON(m, trim.CodeOK)
 }
 
 // roverPath creates a path to a rover resource based on a given rover.
